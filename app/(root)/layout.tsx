1c2cc75f573eda73e4751f0cd7d1046e795c9f5a
mahmoudchemy <realmahmoudcodes@gmail.com>
2025-05-23 21:57:57 +0100

    Update footer component and fix mobile nav toggle

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,18 @@
+import SideBar from "@/components/SideBar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName:'Mahmoud', lastName:'Hamidoun'};
+
   return (
     <html lang="en">
-      <main>
-        SideBar
+      <main className="flex h-screen w-full font-inter">
+        <SideBar
+          user={loggedIn}
+        />
         {children}
       </main>
     </html>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName:'Mahmoud'};
+
   return (
-    <div> Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions effeciently'
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={15800.77}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
