6885818bba4f159f0a6979e238f61e3f134a581b
mahmoudchemy <realmahmoudcodes@gmail.com>
2025-06-07 16:48:15 +0100

    'implement all the changes made to the code'

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import { Pagination } from '@/components/Pagination';
 import TransactionsTable from '@/components/TransactionsTable';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
@@ -6,7 +7,8 @@ import { formatAmount } from '@/lib/utils';
 import React from 'react'
 
 const TransactionHistory = async ({searchParams:{id,page}}:SearchParamProps) => {
-  const currentPage = Number(page as string) || 1;
+  
+    const currentPage = Number(page as string) || 1;
     const loggedIn = await getLoggedInUser();
     const accounts = await getAccounts({ 
       userId: loggedIn.$id 
@@ -19,7 +21,16 @@ const TransactionHistory = async ({searchParams:{id,page}}:SearchParamProps) => 
   
     const account = await getAccount({ appwriteItemId })
   
-    
+      const rowsPerPage = 10;
+      const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
+
+      const indexOfLastTransaction = currentPage * rowsPerPage;
+      const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+      const currentTransactions = account?.transactions.slice(
+        indexOfFirstTransaction, indexOfLastTransaction
+      )
+        
   return (
     <section className='transactions'>
         <div className='transactions-header'>
@@ -55,8 +66,17 @@ const TransactionHistory = async ({searchParams:{id,page}}:SearchParamProps) => 
 
           <section className='flex w-full flex-col gap-6'>
               <TransactionsTable
-                transactions={account.transactions}
+                transactions={currentTransactions}
               />
+
+                          {totalPages>1 && (
+                            <div className='my-4 w-full'>
+                              <Pagination
+                                totalPages={totalPages}
+                                page={currentPage}
+                              />
+                            </div>
+                          )}
           </section>
         </div>
     </section>
